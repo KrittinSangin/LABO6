54c082bf322c93fc74365f3dc7b4ccbd290f004c
Perm <permsangin@gmail.com>
2024-11-29 17:00:14 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
-
+int x=1,odd = 0,even = 0;
 int main(){
+    while(x!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    if(x!=0){
+    if(x%2 == 0 ){
+        even += 1;
+    }else{
+        odd +=1;
+            }
+        }
+    }
+    cout << "#Even numbers = "<<even<<endl;
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
